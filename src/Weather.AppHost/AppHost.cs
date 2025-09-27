4159728e17f269224265bdc0fceada05c0f09259
Al Lamarre <alamarre@gmail.com>
2025-09-27 18:28:44 -0400

    add basic Aspire setup

--- src/Weather.AppHost/AppHost.cs
+++ src/Weather.AppHost/AppHost.cs
@@ -1,3 +1,23 @@
 var builder = DistributedApplication.CreateBuilder(args);
+var postgres = builder.AddPostgres("postgres");
+var pgdb =  postgres.AddDatabase("main");
+
+var sqlServer = builder.AddSqlServer("sqlserver");
+var sqldb = sqlServer.AddDatabase("master");
+
+if(builder.Configuration["EPHEMERAL"] != "true")
+{
+    postgres.WithPgAdmin()
+        .WithLifetime(ContainerLifetime.Persistent);
+    sqlServer
+        .WithLifetime(ContainerLifetime.Persistent);
+}
+
+if (builder.Configuration["TESTING"] != "true")
+{
+     builder.AddProject<Projects.Weather_Api_Core>("WeatherAPI")
+        .WithReference(pgdb)
+        .WithReference(sqldb);
+}
 
 builder.Build().Run();
